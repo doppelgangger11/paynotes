8c0ce8e6b5e7433128edd1f0252825eb81120b53
doppelgangger11 <gamenightmer@gmail.com>
2026-03-20 19:45:01 +0500

    created django models

--- paynotes/paymanager/models.py
+++ paynotes/paymanager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class PaymentModel(models.Model):
+    INCOME_EXPENSES_CHOICES = (
+        ('income', 'income'),
+        ('expenses', 'expenses')
+    )
+
+    date = models.DateField()
+    summ = models.IntegerField()
+    comment = models.TextField(blank=True)
+    is_payed = models.BooleanField(default=False)
+    income_expenses_type = models.CharField(max_length=8, choices=INCOME_EXPENSES_CHOICES, default='expenses')
+
+class PaymentClassesModel(models.Model):
+    payment = models.ForeignKey(PaymentModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
