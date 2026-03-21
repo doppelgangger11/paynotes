d0bd3f7a09407e9b72c7623fda227ad7282e94e3
doppelgangger11 <gamenightmer@gmail.com>
2026-03-22 01:41:18 +0500

    create payment-user link

--- paynotes/paymanager/models.py
+++ paynotes/paymanager/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class PaymentModel(models.Model):
         ('expenses', 'expenses')
     )
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     date = models.DateField()
     summ = models.IntegerField()
     comment = models.TextField(blank=True)
